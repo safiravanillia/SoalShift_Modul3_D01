40e29ac4e0fc9e864d7443eee6b6c6b2eb863503
safiravanillia <32903307+safiravanillia@users.noreply.github.com>
2017-10-24 21:11:19 +0700

    thread terbaca tp masih salah dan ga kluar 1

--- soal5.c
+++ soal5.c
@@ -1,26 +1,29 @@
 #include<stdio.h>
 #include<string.h>
 #include<pthread.h>
+#include<stdlib.h>
+#include<unistd.h>
 
 void *thread_ifah(void *argv);
-//void *thread_fina(void *argv);
-//int status;
+void *thread_fina(void *argv);
+int status;
+pthread_t tid1,tid2;
 
 int main(){
    char run[6], fina[5], ifah[5];
-   pthread_t tid1,tid2;
    scanf("%s %s %s", run, ifah, fina);
 
    pthread_create(&(tid1),NULL,thread_ifah,(void *)&ifah);
-   //pthread_create(&(tid2),NULL,thread_fina,(void *)&fina);
+   pthread_create(&(tid2),NULL,thread_fina,(void *)&fina);
 
    pthread_join(tid1, NULL);
-   //pthread_join(tid2, NULL);
+   pthread_join(tid2, NULL);
    return 0;
 }
 
 void *thread_ifah(void *argv) {
-   //status = 0;
+   status = 0;
+
    char *ifah[5];
    ifah[5]=(char*)argv;
    
@@ -40,11 +43,13 @@ void *thread_ifah(void *argv) {
    fclose(fileIn);
 
    printf("%s : %d\n",ifah[5], count);
-   //status = 1;
-   //return NULL;
+   
+   status = 1;
+   return NULL;
 }
 
-/* while(status!=1)
+void *thread_fina(void *argv) {
+   while(status!=1)
    {
 	
    }
@@ -60,11 +65,11 @@ void *thread_ifah(void *argv) {
 
    fileIn = fopen("/home/safiravanillia/novel.txt", "r");
    while((read = getline(&line, &len, fileIn)) != -1) {
-     if (strstr(line, *fina) != NULL) {
+     if (strstr(line, fina[5]) != NULL) {
        count++;
      }
    fclose(fileIn);
 
-   printf("Fina : %d\n", count);
+   printf("%s : %d\n",fina[5], count);
    }
-}*/
+}
