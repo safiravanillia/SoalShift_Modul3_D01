545e37bdaba3196f5022deb12845b304d512fe11
safiravanillia <32903307+safiravanillia@users.noreply.github.com>
2017-10-25 11:45:07 +0700

    diubah ke dinamis inputannya

--- soal4.c
+++ soal4.c
@@ -5,24 +5,20 @@
 void * thread_fakt(void *);
 
 int main() {
-    int num1,num2,num3,count;
+    int num,count,i;
     char run[13];
     pthread_t tid1,tid2,tid3;
-    //printf("./faktorial ");
-    scanf("%s %d %d %d",run, &num1, &num2,&num3);
+    scanf("%s", run);
 
-    pthread_create(&tid1,NULL,thread_fakt,(void *)&num3);
-    pthread_create(&tid2,NULL,thread_fakt,(void *)&num2);
-    pthread_create(&tid3,NULL,thread_fakt,(void *)&num1);
-   
+    while(scanf("%d", &num) != EOF) {
+    pthread_create(&tid1,NULL,thread_fakt,(void *)&num);
     pthread_join(tid1, NULL);
-    pthread_join(tid2, NULL);
-    pthread_join(tid3, NULL);
+    }
     return 0;
 }
 
 void *thread_fakt(void *argv) {
-    int *num, count;int fakt=1;
+    int *num, count, fakt=1;
     num=(int*)argv;
 
     for(count=1; count<=*num; count++) {
