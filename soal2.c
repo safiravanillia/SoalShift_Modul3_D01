af77fe41c99f66e0c4ac9408f93e8c27a59af266
fariz putra dandi <farizputradandi1@gmail.com>
2017-10-25 12:31:15 +0700

    update 6

--- soal2.c
+++ soal2.c
@@ -6,54 +6,76 @@
  
 pthread_t tid1;
 pthread_t tid2;
-int status;
+int status=0,a,b;
 int nomor;
 int kotakp1[16],kotakp2[16];
- 
-
+char pemain1[100],pemain2[100];	
+int inisial1,inisial2;
 void* player1(void *arg)
-{int inisial1=0;
-    status = 0;
+{ 
+    if(status == 0){
+	printf("selamat datang pemain1 %s\nselamat datang pemain2 %s\n-----------------------------------\n",pemain1,pemain2);
+	/*printf("%s's as player1 turn\nthis is you land mine\n",pemain1);
 	for(int i=0;i<16;i++){
-		printf("%d",kotakp1[i]);		
+		printf("%d ",kotakp1[i]);		
 		}
- 	printf("player1's turn");
-	printf("plant a MINE, in hole number = ");
-	
+	printf("\nplant a MINE, in hole number = ");
+	scanf("%d", &inisial1);
+	kotakp1[inisial1]=1;
    //program
-     
+     system("clear");*/
+	printf("player1 = ");
+	scanf("%d",&a);
+	printf("huuhuhuh\n");
     status = 1;
- 
+ }
     return NULL;
 }
 
 
 void* player2(void *arg)
-{int inisial2=0;
-    while(status = 0){}
- //program
+{
+    
+
+	if(status==1){
+	printf("selamat datang pemain1 %s\nselamat datang pemain2 %s\n-----------------------------------\n",pemain1,pemain2);
+	printf("player2 = ");
+	scanf("%d",&b);
+	printf("hehe\n");
+	/*printf("%s's as player2 turn\nthis is you land mine\n",pemain2);
+	for(int i=0;i<16;i++){
+		printf("%d ",kotakp2[i]);		
+		}
+	printf("\nplant a MINE, in hole number = ");
+	scanf("%d", &inisial2);
+	kotakp2[inisial2]=1;*/
+	status=0;
+	}
+ 
 }
  
 int main(void)
 {	
-	char pemain1[100],pemain2[100];	
+	 
 	memset(kotakp1,0,sizeof(kotakp1));
 	memset(kotakp2,0,sizeof(kotakp2));
     int i=0;
-    while(1){
+
 	printf("masukan nama pemain 1 = ");
   	scanf("%s",pemain1);
 	printf("masukan nama pemain 2 = ");
 	scanf("%s",pemain2);
 	printf("\n");
-	system("clear");
-	printf("selamat datang pemain1 %s\nselamat datang pemain2%s\n",pemain1,pemain2);
+    while(1){	 
+	
+	 
 
     pthread_create(&(tid1), NULL, &player1, (void*)pemain1);
     pthread_create(&(tid2), NULL, &player2, (void*)pemain2);
- 
-    pthread_join(tid1, NULL);
+ pthread_join(tid1, NULL);
     pthread_join(tid2, NULL);
+     
  }
+
     return 0;
 }
