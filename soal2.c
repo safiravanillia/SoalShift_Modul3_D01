e6b2812d87deb3f1631585759e72ce88ff60c499
fariz putra dandi <farizputradandi1@gmail.com>
2017-10-25 13:18:19 +0700

    update fix

--- soal2.c
+++ soal2.c
@@ -6,22 +6,45 @@
  
 pthread_t tid1;
 pthread_t tid2;
-int status=0,a,b;
+int status=0,a,b,n=0;
 int nomor;
 int kotakp1[16],kotakp2[16],poin1=0,poin2=0;
 char pemain1[100],pemain2[100];	
 int inisial1,inisial2;
 void* player1(void *arg)
 { 
+ 
+ 
     if(status == 0){
+//tebak
+	if(n>0){//soalnya pertama kan kita masang bomb, bukan nebak
+	printf("%s's turn as player1, poin = %d \n",pemain1,poin1);
+	int tebak1;
+	printf("tebak 4 kotak milik player 2 yang tidak ada bombnya = ");
+	for(int a=0;a<4;a++){	
+		scanf("%d",&tebak1);
+		if(kotakp2[tebak1]==1){
+			poin2+=1;
+			printf("terdapat bomb pada lubang ke %d\n",tebak1);		
+		}
+		else if(kotakp2[tebak1]==0){
+			poin1+=1;
+			printf("tidak ada bomb pada lubang ke %d\n",tebak1);
+		}
+	}
+	printf("total poin %s = %d \ntotal poin %s = %d\n",pemain1,poin1,pemain2,poin2);
+	printf("\n---------------------------------\n");
+	printf("\n\n");
+}
+//tanam
 	int bnyk1;
-	printf("%s's turn as player1  \nthis is you land mine\n",pemain1);
+	printf("this is %s's land mine\n",pemain1);
 	for(int i=0;i<16;i++){
 		printf("%d ",kotakp1[i]);		
 		}
 	printf("\nhow much MINE yg pgn di tanem = ");
 	scanf("%d",&bnyk1);
-	printf("\nplant a MINE, in hole number = ");
+	printf("plant a MINE, in hole number = ");
 	for(int a=1;a<=bnyk1;a++){
 		scanf("%d", &inisial1);
 		kotakp1[inisial1]=1;	
@@ -32,7 +55,7 @@ void* player1(void *arg)
 		}
 	printf("\n---------------------------------\n");
 	printf("\n\n");
-	 
+	 n++;
     status = 1;
  }
     return NULL;
@@ -45,19 +68,23 @@ void* player2(void *arg)
 
 	if(status==1){
 //tebak bom
+	printf("%s's turn as player2,poin = %d  \n",pemain2,poin2);
 	int tebak2;
-	printf("tebak 4 kotak milik player 1 yang tidak ada bombnya");
+	printf("tebak 4 kotak milik player 1 yang tidak ada bombnya = ");
 	for(int a=0;a<4;a++){	
 		scanf("%d",&tebak2);
 		if(kotakp1[tebak2]==1){
 			poin1+=1;
-			printf("terdapat bomb pada lubang ke %d",tebak2);		
+			printf("terdapat bomb pada lubang ke %d\n",tebak2);		
 		}
 		else if(kotakp1[tebak2]==0){
 			poin2+=1;
+			printf("tidak ada bomb pada lubang ke %d\n",tebak2);
 		}
 	}
-	
+	printf("total poin %s = %d \ntotal poin %s = %d\n",pemain1,poin1,pemain2,poin2);
+	printf("\n---------------------------------\n");
+	printf("\n\n");
 
 
 
@@ -72,7 +99,7 @@ void* player2(void *arg)
 
 //untuk input bomb
 	int bnyk2;
-	printf("%s's turn as player2  \nthis is you land mine\n",pemain2);
+	printf("this is %s's land mine\n",pemain2);
 	for(int i=0;i<16;i++){
 		printf("%d ",kotakp2[i]);		
 		}
